a619df9fb0c27f23e823331d02e37372a914c299
Lentils <alan.lenti@gmail.com>
2025-02-06 12:13:30 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,44 +1,27 @@
 //Declares variables 
 let rocks = []; // This is the rock array in which the rock objects are placed in
 let player1;
-let rocksNum = 5 
+let rocksNum = 3
 let t = 0;
 let deathMessage = "Game Over. "
 let margin = 0;
-
-//This creates 3 variables to be able to change the RGB value of the background 
-let colr = 128;
-let colg = 128;
-let colb = 128;
+let missed = 0;
   
 function setup() {
   createCanvas(700, 700);
   
-  // This for statement creates rocksNum amount of rocks and places them within the array 
-  for (let i = 0; i < rocksNum; i++){
-   rocks[i] = new Rock (random(0, width), random(0, 100), color(random(255)))
-    
-}
-  
-  player1 = new Player(); //This creates the player 
-}
+  // This for statement creates rocksNum amount of rocks and places them within the array
+  for (let i = 0; i < rocksNum; i++ ){
+    rocks[i] = new Rock (random(30, width - 30), random(0, 70))
+  }
+
+    player1 = new Player(); //This creates the player 
+  }
+
 
 function draw() {
-
-  background(colr, colg, colb);
   
-  //This is the clock which counts up every second, allowing for a score of how long you have survived
-   if (frameCount % 60 ==0) {
-    t++}
-  
-  //This creates a flashing background and changes the death message once over a score of 100
-  if (t > 100) {
-    colr = random(0, 255)
-    colg = random(0, 255)
-    colb = random(0, 255)
-    deathMessage = "Did your eyes hurt? "
-    margin = 50;
-  }
+  background(256);
   
   //This calls all the functions for the rocks, enabling their properties
   for (let i = 0; i < rocksNum; i++){
@@ -57,15 +40,19 @@ function draw() {
   textSize(20)
   text("Score: " + t, 10, 20)
   
+  fill(0, 0, 0)
+  textSize(20)
+  text("Missed: " + missed, 10, 40)
+  
 }
 
   //This is the Player class 
 class Player {
   constructor(){
     this.x = width/2;
-    this.y = height - 50;
-    this.w = 30;
-    this.h = 30;
+    this.y = height - 100;
+    this.w = 50;
+    this.h = 20;
     this.c = color(255,0,0);
   }
   
@@ -77,27 +64,25 @@ class Player {
   
   //This function allows the player to move based oon the mouse coordinates
   move(){
-    
-    this.x = mouseX - 15}
+    this.x = mouseX - player1.w/2}
   
-  //This ensures the player stays within the confines of the canvas by resetting its position by its width.
+  //This ensures the player stays within the confines of the canvas by resetting its position by   its width.
   home(){
     if (this.x < 0){
       this.x = 0}
     
     if (this.x > width)
-      this.x = width - 30}
+      this.x = width - player1.w}
 }
 
   //This creates the Rock class
-class Rock {
+  class Rock {
   
-  constructor(x, y, c){  
+  constructor(x, y){  
     this.x = x;
     this.y = y;
     this.w = 30;
     this.h = 16;
-    this.c = c;
 }
   
   //This creates the shape of the rock and colors it
@@ -106,22 +91,31 @@ class Rock {
     text('💣', this.x, this.y)
   }
   
-  //This moves the rock and increases its speed by basing it on a constantly increasing frameCount
+  //This moves the rock and increases its speed by basing it on a constantly increasing             frameCount
   move(){
-    this.y += (frameCount * 0.0025);
+    let fallRate = 0.0025;
+    
+    if (missed < 5) {
+    this.y += (frameCount * fallRate)}
+    
+    else (this.y = -30);
     
   //This resets the rock back to the top of the canvas
     if (this.y > height){
       this.y = 0;
-      this.x = random(width)}
+      this.x = random(width)
+      missed ++
+    }
   }
   
   //This checks for collsions by checking if the corners of the player are within the area of the rock.
   checkCollision(){
-    if (player1.x + player1.w/2 > this.x && player1.x < this.x + this.w && player1.y + player1.h/2 > this.y && player1.y < this.y + this.h){
+    if (player1.x + player1.w/2 > this.x  && player1.x - player1.w/2 < this.x + this.w &&             player1.y + player1.h/2 > this.y && player1.y < this.y + this.h){
       
       this.y = 0;
       this.x = random(width) 
+      
+      t++
     } 
   } 
 }
